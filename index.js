f8d84d9098bf551d7b227f05794a00bb245d4541
NobelAI <7415805+NobelAI@users.noreply.github.com>
2018-12-11 11:45:36 +0530

    bug fixes

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ con.connect(function(err) {
 });
  
 var created=new Date();
-var server = app.listen(80,'139.59.6.36', function () {
+var server = app.listen(80,'206.189.142.47', function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Server listening at http://%s:%s", host, port)
